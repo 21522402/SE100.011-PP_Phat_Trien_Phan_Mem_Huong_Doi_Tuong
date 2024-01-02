b7e260be82651b9c5f9a15738473eb7edb24c770
Huynh Ngoc Qui <quichua333@gmail.com>
2024-01-02 17:50:55 +0700

    fix

--- HotelManagement/View/Staff/RoomCatalogManagement/RoomInfo/RoomRentalContractInfo.xaml.cs
+++ HotelManagement/View/Staff/RoomCatalogManagement/RoomInfo/RoomRentalContractInfo.xaml.cs
@@ -5,12 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace HotelManagement.View.Staff.RoomCatalogManagement.RoomInfo
 {
@@ -43,13 +40,6 @@ namespace HotelManagement.View.Staff.RoomCatalogManagement.RoomInfo
             
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            
-            PrintDialog printDialog = new PrintDialog();
-           
-                printDialog.PrintVisual(invoice, "Invoice");
-           
-        }
+       
     }
 }
